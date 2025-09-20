853384f59d97e8c516dbae66c5474178a53935f3
Cian Butler <butlerx@notthe.cloud>
2025-09-20 21:03:02 +0100

    New Header Image

--- src/components/header.rs
+++ src/components/header.rs
@@ -8,12 +8,17 @@ pub fn header() -> Html {
     let onclick = Callback::from(move |_| navigator.push(&Route::Home));
     html! {
         <h1 class="header">
-            <img {onclick} src="./static/icon-128x128.png" alt="Jet Lag Icon" class="header-icon" />
-            <span>
-                { "Jet Lag: The Game - Hide + Seek" }
-                <br />
-                { "Investigation Book" }
-            </span>
+            <div class="logo-container" {onclick}>
+                <div class="main-logo">
+                    <span class="logo-text">{ "jet" }</span>
+                    <div class="logo-icon">
+                        <img src="./static/icon-128x128.png" alt="Jet Lag Icon" class="icon" />
+                    </div>
+                    <span class="logo-text">{ "lag" }</span>
+                </div>
+                <div class="maps-label">{ "The Game - Hide + Seek" }</div>
+            </div>
+            <span class="subtitle">{ "Investigation Book" }</span>
         </h1>
     }
 }
